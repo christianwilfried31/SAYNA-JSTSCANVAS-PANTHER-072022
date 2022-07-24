776dc475cd64c4cd9dd4c3345ddbeb7eef1bb69e
Christian Wilfried <44750483+christianwilfried31@users.noreply.github.com>
2022-07-24 13:55:00 +0200

    Seconde évaluation

---
+++ assets/js/wakanda.js
@@ -0,0 +1,50 @@
+const titles = document.querySelectorAll('h1');
+const subtitles = document.querySelectorAll('h3');
+const imgs = document.querySelectorAll('img');
+const paras = document.querySelectorAll('p');
+const navbar = document.querySelector('nav');
+const sitemap = document.querySelector('.sitemap');
+const bars = document.querySelectorAll('.bar');
+const video = document.querySelector('.video');
+
+const scrollFadeIn = [
+  { transform: 'translateX(-1000px) scale(0.1)'},
+  { transform: 'translateX(0) scale(1)'}
+];
+
+const scrollFadeInBar = [
+  { transform: 'translateY(-1000px) scale(0.1)'},
+  { transform: 'translateY(0) scale(1)'}
+];
+
+const scrollTiming = {
+  duration: 1000,
+  scrollEasing: 'easeOut'
+}; 
+
+window.addEventListener('scroll', () => { 
+  bars.forEach(bar => {
+    bar.animate(scrollFadeInBar, scrollTiming);
+  });
+
+  navbar.animate(scrollFadeIn, scrollTiming);
+
+  titles.forEach(title => {
+    title.animate(scrollFadeIn, scrollTiming);
+  });
+
+  subtitles.forEach(subtitle => {
+    subtitle.animate(scrollFadeIn, scrollTiming);
+  });
+
+  imgs.forEach(img => {
+    img.animate(scrollFadeIn, scrollTiming);
+  });
+
+  paras.forEach(para => {
+    para.animate(scrollFadeIn, scrollTiming);
+  });
+
+  sitemap.animate(scrollFadeIn, scrollTiming);
+  video.animate(scrollFadeIn, scrollTiming);
+});
